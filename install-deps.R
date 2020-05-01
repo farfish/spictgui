3f3c339983554180bb738014725aee043ff8cd5e
Jamie Lentin <jm@lentin.co.uk>
2020-05-01 12:07:16 +0100

    install-deps: Use shinycssloaders that doesn't fall over id-less-inputs

--- install-deps.R
+++ install-deps.R
@@ -1,6 +1,7 @@
 install.packages("pool")
 devtools::install_github("thomasp85/patchwork")
 devtools::install_github("r-lib/memoise")
+remotes::install_github("daattali/shinycssloaders", "197ef14")
 install.packages("shinycssloaders")
 
 # Develpoment dependencies
